321219d192a9029ee307b3e9447eeb91db870518
Gokul Ram <ramgokul637@gmail.com>
2026-03-17 02:24:58 +0530

    fix: updated server.js for deployment

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+
+// const authRoutes = require("./routes/authRoutes");
+// const productRoutes = require("./routes/productRoutes");
+// const cartRoutes = require("./routes/cartRoutes");
+// const orderRoutes = require("./routes/orderRoutes");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/cart", cartRoutes);
+// app.use("/api/orders", orderRoutes);
+
+// mongoose.connect("mongodb://127.0.0.1:27017/productDB")
+// .then(()=>console.log("MongoDB Connected"))
+// .catch(err=>console.log(err));
+
+// app.listen(5000,()=>{
+// console.log("Server running on port 5000");
+// });
+
+
+
+
+require("dotenv").config(); // 1. Ithu thaan top-la irukanum
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -17,10 +48,13 @@ app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 
-mongoose.connect("mongodb://127.0.0.1:27017/productDB")
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
+// 2. Local link-ah remove pannitu process.env.MONGO_URI kudukanum
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("✅ MongoDB Connected to Atlas"))
+.catch(err=>console.log("❌ DB Connection Error:", err));
 
-app.listen(5000,()=>{
-console.log("Server running on port 5000");
+// 3. Port-ah dynamic-ah mathanum (Render-kaga)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
 });
